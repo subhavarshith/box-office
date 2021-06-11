b913a4a16614bd42fc58487ace507c3171acfaae
subha <mohansmarty1@gmail.com>
2021-06-11 17:34:00 +0530

    completed project

---
+++ src/components/Navs.js
@@ -0,0 +1,31 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import { useLocation } from 'react-router';
+import { NavList, LinkStyled } from './Navs.styled';
+
+const LINKS = [
+  { to: '/', text: 'Home' },
+  { to: '/starred', text: 'Starred' },
+];
+const Navs = () => {
+  const location = useLocation();
+
+  return (
+    <div>
+      <NavList>
+        {LINKS.map(item => (
+          <li>
+            <LinkStyled
+              to={item.to}
+              className={item.to === location.pathname ? 'active' : ''}
+            >
+              {item.text}
+            </LinkStyled>
+          </li>
+        ))}
+      </NavList>
+    </div>
+  );
+};
+
+export default Navs;
